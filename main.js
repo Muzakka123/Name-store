c7d223b6224f6e3ee9b0ee42174720a862ba81b0
Muzakka123 <159703338+Muzakka123@users.noreply.github.com>
2024-05-12 16:19:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// the names of a few of your friends in a array called names. 
+// Print each person’s name by accessing each element in the list, one at a time.
+var members = ["muzakka", "maryam", "hanina", "amna", "muneeb",];
+console.log(members[0]);
+console.log(members[1]);
+console.log(members[2]);
+console.log(members[3]);
+console.log(members[4]);
